420530c3f7e4900925e0b9f16e3fff32b7eaa066
boxosoup <jhi8g7dvsa8ahb@gmail.com>
2026-03-12 19:23:45 -0400

    Add files via upload

--- ModEntry.cs
+++ ModEntry.cs
@@ -10,6 +10,7 @@ using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley.Menus;
 using StardewValley.Objects;
+using static StardewValley.Game1;
 
 namespace Birthday;
 
@@ -17,32 +18,56 @@ namespace Birthday;
 public class ModEntry : Mod
 {
     public static string UniqueID = "boxosoup.birthday";
+    private ModConfig Config;
     public override void Entry(IModHelper helper)
     {
         // bind the event handler
         I18n.Init(helper.Translation);
         helper.Events.Input.ButtonPressed += OnButtonPressed;
         helper.Events.GameLoop.DayStarted += OnDayStarted;
+        
+        this.Config = this.Helper.ReadConfig<ModConfig>();
+        string exampleBool = this.Config.birthdaymonth;
     }
-
+    
     void OnButtonPressed(object? sender, ButtonPressedEventArgs ev)
     {
         // Don't process button presses if player hasn't loaded a save,
         // is in another menu, or isn't free. I'd recommended you ignore these cases too.
         if (!Context.IsWorldReady) return;
-        if (Game1.activeClickableMenu != null || (!Context.IsPlayerFree)) return;
+        if (activeClickableMenu != null || (!Context.IsPlayerFree)) return;
         
         // Display our UI if user presses F10
         if (ev.Button == SButton.W)
-            Game1.activeClickableMenu = new BirthdayUi();
+            activeClickableMenu = new BirthdayUi();
     }
     
     private void OnDayStarted(object? sender, DayStartedEventArgs e)
     {
-        if (Game1.activeClickableMenu != null || (!Context.IsPlayerFree)) return;
+        if (activeClickableMenu != null || !Context.IsPlayerFree)
+            return;
+
         if (!GameStateQuery.Exists($"{UniqueID}_IS_BIRTHDAY"))
         {
-            Game1.activeClickableMenu = new BirthdayUi();
-        } ;
+            activeClickableMenu = new BirthdayUi();
+        }
+        else
+        {
+            if (player.modData.TryGetValue($"{ModEntry.UniqueID}/birthdaydate", out string birthdaydata))
+            {
+                int dayofbirthday = int.Parse(birthdaydata.Substring(birthdaydata.Length - 2));
+
+                if (birthdaydata.Contains(currentSeason) && dayOfMonth == dayofbirthday)
+                {
+                    Game1.player.activeDialogueEvents.TryAdd($"{UniqueID}_birthday", 1);
+                }
+            }
+        }
+    }
+    
+    public sealed class ModConfig
+    {
+        public string birthdaymonth { get; set; }
+        public int birthdayday { get; set; }
     }
 }
